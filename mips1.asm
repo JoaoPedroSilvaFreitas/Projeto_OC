b8d2a401e2d670576d1616834ef7ad535f7fe059
João <freitas.joaopedro@estudante.ufjf.br>
2023-05-11 12:50:27 -0300

    organizando Labels

--- mips1.asm
+++ mips1.asm
@@ -2,10 +2,27 @@
 	vet: .space 80
 	
 	addi $0, $zero, 0
-	
+
+zeraVetor:
+
+jr $ra
+
+valorAleatorio:
+
+jr $ra
+
+imprimeVetor:
+
+jr $ra
+
+troca:
+
+jr $ra
 	
 inicializaVetor:
 
+jr $ra
 	
+			
 main:
 	
