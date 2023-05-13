416f45c7c804778ce31f1501f344f1e9bcf9ed70
João Vitor <jv155gomes@gmail.com>
2023-05-13 14:09:35 -0300

    modificacao

--- mips2.asm
+++ mips2.asm
@@ -22,7 +22,8 @@
 		add $t1,$t0,$a2
 		div $t1, $a3
 		mfhi $t2
-		sub $v0, $t2, $at
+		lw $t3,0($sp)
+		sub $v0, $t2, $t3
 
 		jr $ra
 	
@@ -30,19 +31,46 @@
 	inicializaVetor:
 		slt $t0, $zero, $a1
 		beq $t0,$zero,FIM
-		la $v0, vet 
+#		la $v0, vet 
+		move $t7,$a1
+		move $t1, $a0
+		move $a0,$a2
 		
-		li $a0 , $a3
 		li $a1, 47
 		li $a2, 97
 		li $a3, 337
-		li $at, 3
+		li $t0, 3
+		
+		addi $sp, $sp, -4
+		sw $ra, 0($sp)
+		addi $sp, $sp, -4
+		sw  $t1, 0($sp)
+		addi $sp, $sp, -4
+		sw $t7, 0($sp)
+		addi $sp, $sp, -4
+		sw $t0 0($sp)
 		
 		jal valorAleatorio
 		
-		move $t0, $v0
-		addi $t1, $a3, -1
-		addi $t2($a2), $t0, 0
+		
+		
+		
+		addi $sp, $sp, 4
+		lw $t3, 0($sp)
+		addi $sp, $sp, 4
+		lw $t0, 0($sp)
+		
+		la $t5, ($t0)
+		
+		
+		move $t1, $v0
+		addi $t3,$t3 , -1
+		li $t4, 4
+		mult $t3, $t4
+		mflo $t3
+		#move $v0, $t4
+		jal imprimeVetor
+		sw $t1, 0($t0)
 		jr $ra	
 		FIM:
 		addi $v0, $zero, 0
@@ -56,7 +84,7 @@
 	.main:
 		la $a0, vet
 		addi $a1,$zero,20
-		addi $a2, 80($a0),0
+		sw $a2, 80($a0)
 		jal inicializaVetor
 		
 		add $s0, $zero,$v0
@@ -64,7 +92,7 @@
 		sw $t0,80($s0)
 	
 		lw $a0, 80($s0)
-		jal imprimeVetor
+		#jal imprimeVetor
 		
 
 
